2949efc203720c9ad4d688f17cf2a441e1ab8a82
Mehmet Efe Alpargan <efe-632006@hotmail.com>
2024-05-06 07:05:41 +0300

    database ui connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,21 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/79f2cab3-a18c-46b4-a8f7-a78e8a76a2bd-rcma58.jpg",
-  "https://utfs.io/f/b4febf6e-94f8-47cb-be8f-b912a7a3fed5-rcma3p.jpg",
-  "https://utfs.io/f/28954bb6-9be0-49b3-8f0c-c1d0ea48e91e-rebmcf.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy:(model, { desc }) => desc(model.id),
+  });
 
   // console.log(posts);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+        {[...images, ...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 flex flex-col">
             <img src={image.url}/>
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
@@ -18,11 +21,12 @@ import {
  */
 export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
 
-export const posts = createTable(
-  "post",
+export const images = createTable(
+  "image",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url", { length: 1024 }).notNull(),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
