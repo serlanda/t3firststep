353beb326bffab54ff08b905f3770d4e3fe5b166
Mehmet Efe Alpargan <efe-632006@hotmail.com>
2024-05-06 06:43:12 +0300

    database connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/79f2cab3-a18c-46b4-a8f7-a78e8a76a2bd-rcma58.jpg",
@@ -10,12 +11,19 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  // console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url}/>
           </div>
         ))}
